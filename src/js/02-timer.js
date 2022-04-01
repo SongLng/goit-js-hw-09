c7ac8123877d9c39e8b4171b145b76d304294f8b
Sergio Pieshko <sergio.pieshko@icloud.com>
2022-04-01 17:43:25 +0300

    doesnt work

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,15 +2,20 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
-};
+const inputEl = document.querySelector('#datetime-picker');
+const buttonEl = document.querySelector('button[datda-start');
+const timerDiv = document.querySelector('.timer');
+const daysEl = document.querySelector('span[data-days');
+const hoursEl = document.querySelector('span[data-hours]');
+const minutesEl = document.querySelector('span[data-minutes]');
+const secondsEl = document.querySelector('span[data-seconds]');
+
+buttonEl.classList.add('disabled');
+let userDate = null;
+
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -20,13 +25,13 @@ function convertMs(ms) {
   const day = hour * 24;
 
   // Remaining days
-  const days = Math.floor(ms / day);
+  const days = pad(Math.floor(ms / day));
   // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
+  const hours = pad(Math.floor((ms % day) / hour));
   // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
   // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
 }
@@ -35,57 +40,53 @@ console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
 console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
 console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
 
-// const isDisabled = true;
-// let selectedDate = Date.now();
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < Date.now()) {
+      Notify.failure('Choose a date');
+      userDate = new Date();
+    } else {
+      buttonEl.disabled = false;
+      buttonEl.classList.remove('disabled');
+      userDate = selectedDates[0];
+    }
+  },
+};
 
-// refs.startBtn.disabled = isDisabled;
-// refs.startBtn.addEventListener('click', onStartClick);
+class Timer {
+  constructor() {
+    this.isActive = false;
+    this.timerId = null;
+    buttonEl.disabled = true;
+  }
+  timerStart() {
+    if (this.isActive) {
+      return;
+    }
+    this.isActive = true;
+    this.timerId = setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = userDate - currentTime;
+      const components = convertMs(deltaTime);
 
-// const flatp = flatpickr(refs.inputEl, options);
-
-// function onCloseFunction(date) {
-//   if (Date.now() > date) {
-//     Notify.failure('Please choose Your date');
-//   } else {
-//     refs.startBtn.disabled = !isDisabled;
-//     selectedDate = date;
-//   }
-// }
-
-// function onStartClick() {
-//   refs.startBtn.disabled = isDisabled;
-//   flatp.destroy();
-//   refs.inputEl.disabled = isDisabled;
-//   summaryStart();
-// }
-
-// class Timer {
-//   constructor() {
-//     this.isActive = false;
-//     this.timerId = null;
-//     buttonEl.disabled = true;
-//   }
-//   timerStart() {
-//     if (this.isActive) {
-//       return;
-//     }
-//     this.isActive = true;
-//     this.timerId = setInterval(() => {
-//       const currentTime = Date.now();
-//       const deltaTime = userDate - currentTime;
-//       const components = convertMs(deltaTime);
-
-//       secondsEl.textContent = components.seconds;
-//       minutesEl.textContent = components.minutes;
-//       hoursEl.textContent = components.hours;
-//       daysEl.textContent = components.days;
-//       if (deltaTime <= 0) {
-//         this.timerStop();
-//         timerDiv.innerHTML = 'Time is over!';
-//       }
-//     }, 1000);
-//   }
-//   timerStop() {
-//     clearInterval(this.timerId);
-//   }
-// }
+      secondsEl.textContent = components.seconds;
+      minutesEl.textContent = components.minutes;
+      hoursEl.textContent = components.hours;
+      daysEl.textContent = components.days;
+      if (deltaTime <= 0) {
+        this.timerStop();
+        timerDiv.innerHTML = 'Time is up';
+      }
+    }, 1000);
+  }
+  timerStop() {
+    clearInterval(this.timerId);
+  }
+}
+const timer = new Timer();
+flatpickr(inputEl, options);
+buttonEl.addEventListener('click', () => timer.timerStart());
